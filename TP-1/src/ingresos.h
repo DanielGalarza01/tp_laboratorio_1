8888c3710bf05caf34c8c055c3ba3d67a4a39abf
DanielGalarza01 <galarzadanieleduardo@gmail.com>
2021-09-25 00:16:54 -0300

    fin2

--- TP-1/src/ingresos.h
+++ TP-1/src/ingresos.h
@@ -8,8 +8,16 @@
 #ifndef INGRESOS_H_
 #define INGRESOS_H_
 
+/// @brief Pide al usuario el ingreso de un número entero y lo devuelve por parámetro.
+///
+/// @param mensaje (Mensaje que sera mostrado al usuario pidiendole que ingrese el número entero)
+/// @return (Devuelve el número entero)
 int IngresarNumeroEntero(char mensaje[]);
 
+/// @brief Pide al usuario el ingreso de un número flotante y lo devuelve por parámetro.
+///
+/// @param mensaje (Mensaje que sera mostrado al usuario pidiendole que ingrese el número flotante)
+/// @return (Devuelve el número flotante)
 float IngresarNumeroFlotante(char mensaje[]);
 
 #endif /* INGRESOS_H_ */
